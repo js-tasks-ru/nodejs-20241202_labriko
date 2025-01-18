d2dbe46e769128a7a17639f1bf47ecc7b0cccc1a
Alexander Teliatnikov <labriko453@gmail.com>
2025-01-18 19:42:33 +0000

    feat(03-di/01-notification-service): Implement NotificatonService

--- 03-di/01-notification-service/notifications/notifications.service.ts
+++ 03-di/01-notification-service/notifications/notifications.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  constructor() {}
+
+  sendEmail(to: string, subject: string, message: string) {
+    if (!to) {
+      throw new BadRequestException()
+    }
+    console.log(`Email sent to ${to}: [${subject}] ${message}`);
+  }
+
+  sendSMS(to: string, message: string) {
+    if (!to) {
+      throw new BadRequestException()
+    }
+    console.log(`SMS sent to ${to}: ${message}`);
+  }
+}
